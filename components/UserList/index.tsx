b72b494fe5d144b545523b653f312b30065efb25
rudis <rudislpp@gmail.com>
2020-10-20 19:41:41 -0300

    user list component

--- components/UserList/index.tsx
+++ components/UserList/index.tsx
@@ -1,17 +1,124 @@
+/* eslint-disable camelcase */
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import _ from "lodash";
 import UserRow from "../UserRow";
+import { Header, Container } from "./styles";
+import { getUser } from "../../services/githubApi";
 
-import { Container } from './styles';
+type UserListProps = {
+  data: [];
+  fetchUsers: (arg?) => void;
+};
 
-const UserList: React.FC = ({ data }) => (
-  <Container>
-    <input placeholder="Pesquisar usuario" />
-    <ul>
-      {data.map(({ login, avatar_url }) => (
-        <UserRow key={login} username={login} photo={avatar_url} />
-      ))}
-    </ul>
-  </Container>
-);
+const UserList: React.FC<UserListProps> = ({ data, fetchUsers }) => {
+  const [uList, setUList] = React.useState([]);
+  const [inputText, setInputText] = React.useState("");
+  const [userNotFound, setUserNotFound] = React.useState(false);
+  const [loadingSearch, setLoadingSearch] = React.useState(true);
+  const [searchData, setSearchData] = React.useState(null);
+  const [state, setState] = React.useState({ hasMore: true });
+  const inputRef = React.useRef();
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSearch = React.useCallback(async () => {
+    setLoadingSearch(true);
+
+    try {
+      const result = await (await getUser(inputText)).json();
+
+      if (result.message === "Not Found") {
+        setLoadingSearch(false);
+        setUserNotFound(true);
+        return;
+      }
+      setSearchData(result);
+      setUserNotFound(false);
+      setLoadingSearch(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [inputText]);
+
+  const delayedQuery = React.useCallback(_.debounce(handleSearch, 500), [
+    inputText,
+  ]);
+
+  React.useEffect(() => {
+    if (localStorage.getItem("search")) {
+      inputRef.current.value = localStorage.getItem("search");
+      setInputText(localStorage.getItem("search"));
+    }
+
+    if (inputText.length > 0) {
+      delayedQuery();
+    }
+
+    return delayedQuery.cancel, localStorage.removeItem("search");
+  }, [inputText, delayedQuery]);
+
+  const fetchMoreData = React.useCallback(async () => {
+    if (uList.length >= 100) {
+      setState({ hasMore: false });
+      return;
+    }
+    await fetchUsers(data.length + 10);
+    setUList([uList, ...data]);
+  }, [data, fetchUsers, uList]);
+
+  React.useEffect(() => {
+    setUList(data);
+  }, [data]);
+
+  return (
+    <Container>
+      <Header>
+        <h2>Search Results</h2>
+        <input
+          ref={inputRef}
+          placeholder="Pesquisar usuario"
+          name="search"
+          onChange={onChange}
+        />
+      </Header>
+      <ul>
+        {inputText.length > 0 && loadingSearch && !userNotFound && (
+          <p style={{ textAlign: "center", padding: 40 }}>
+            <b>Loading...</b>
+          </p>
+        )}
+        {inputText.length > 0 && !loadingSearch && !userNotFound && (
+          <UserRow username={searchData.login} photo={searchData.avatar_url} />
+        )}
+        {inputText.length > 0 && !loadingSearch && userNotFound && (
+          <p style={{ textAlign: "center", padding: 40 }}>
+            <b>Usuário não encontrado</b>
+          </p>
+        )}
+        {inputText.length === 0 && (
+          <InfiniteScroll
+            height={400}
+            dataLength={uList.length}
+            next={() => fetchMoreData()}
+            hasMore={state.hasMore}
+            loader={<h4>Carregando mais usuarios...</h4>}
+            endMessage={
+              <p style={{ padding: 20, textAlign: "center" }}>
+                <b>Yay! Você viu tudo..</b>
+              </p>
+            }
+          >
+            {uList.map(({ login, avatar_url }, i) => (
+              <UserRow key={login + i} username={login} photo={avatar_url} />
+            ))}
+          </InfiniteScroll>
+        )}
+      </ul>
+    </Container>
+  );
+};
 
 export default UserList;
--- components/UserList/styles.ts
+++ components/UserList/styles.ts
@@ -1,4 +1,48 @@
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
+import { breakpoints } from "../util";
+
+export const desktopContainer = css`
+  max-width: 860px;
+  min-height: 600px;
+  margin: 40px auto;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  height: 100%;
+
+  .infinite-scroll-component {
+    display: flex;
+    max-width: 700px;
+    margin: 0 auto;
+    flex-wrap: wrap;
+    padding: 36px;
+    overflow: scroll;
+
+    li {
+      max-width: 140px;
+      height: 215px;
+      flex-direction: column;
+      background: #ffffff;
+      border: 1px solid #dedede;
+      box-sizing: border-box;
+      margin: 0;
+
+      img {
+        width: 100px;
+        height: 100px;
+        margin: 0;
+      }
+      p {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 36px;
+        text-align: center;
+        color: #162668;
+      }
+    }
+  }
+`;
 
 export const Container = styled.div`
   display: flex;
@@ -9,11 +53,20 @@ export const Container = styled.div`
 
   input {
     background: #ffffff;
-    box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.5);
+    border: 1px solid #dedede;
     border-radius: 5px;
     padding: 8px;
     width: 94%;
-    margin-bottom: 42px;
+
+    @media (min-width: ${breakpoints.desktop.min}) {
+      margin-right: 51px;
+      height: 35px;
+      padding: 0 8px;
+      width: 245px;
+      background: #ffffff;
+      border: 1px solid #dedede;
+      border-radius: 5px;
+    }
   }
 
   ul {
@@ -22,5 +75,36 @@ export const Container = styled.div`
     justify-content: center;
     flex-direction: column;
     width: 100%;
+    div {
+      width: inherit;
+    }
+  }
+
+  @media (min-width: ${breakpoints.desktop.min}) {
+    ${desktopContainer}
+  }
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px 0 32px 0;
+  margin: 0 19px;
+  @media (min-width: ${breakpoints.desktop.min}) {
+    border-bottom: 1px solid #ededed;
+  }
+
+  h2 {
+    font-weight: 600;
+    font-size: 30px;
+    line-height: 36px;
+    color: #000000;
+    margin-left: 33px;
+    display: none;
+    @media (min-width: ${breakpoints.desktop.min}) {
+      display: unset;
+    }
   }
 `;
